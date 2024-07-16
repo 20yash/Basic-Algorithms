185c239debe3f62328f90a4737c7659cb38b99d5
shivanshu pathak <kashyapy58@gmail.com>
2024-07-16 15:29:45 +0530

    merge sort practice

--- mergeSort.js
+++ mergeSort.js
@@ -2,28 +2,26 @@ function mergeSort(arr){
     if(arr.length<2){
         return arr
     }
-    const mid = Math.floor(arr.length/2)
-    const left = arr.slice(0,mid)
-    const right = arr.slice(mid)
+    const middle = Math.floor(arr.length/2)
+    const left = arr.slice(0,middle)
+    const right = arr.slice(middle)
+
     return merge(mergeSort(left),mergeSort(right))
 }
 
 function merge(left,right){
-    const temp=[]
-    while(left.length && right.length)
-    {
-       
-        if(left[0]<right[0]){
+
+    const temp = []
+    while(left.length && right.length){
+        if(left[0]<=right[0]){
             temp.push(left.shift())
         }
-        else{
+        else
             temp.push(right.shift())
-        }
-    
-}
+    }
     return [...temp,...left,...right]
 }
 
-const arr=[33,4,23,12,65,7,45]
+const arr= [33,2,32,45,65,47,88,9,10]
 
-console.log(mergeSort(arr));
+console.log(mergeSort(arr))
